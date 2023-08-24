c0c20f60c9dff9ee5cffd160086c037647656d24
Abasiekeme Hanson <97063739+Tblqck@users.noreply.github.com>
2023-08-24 15:00:09 +0100

    Add files via upload

---
+++ T black/Arbitrage bot/exchange_markets.py
@@ -0,0 +1,21 @@
+# exchange_markets.py
+
+exchange_market_lists = {
+    'Binance': {
+        'MATIC': 'MATICUSDT',
+        'BTC': 'BTCUSDT',
+        'ETH': 'ETHUSDT',
+        # Add or modify more assets and trading pairs for Binance
+    },
+    
+#---------------------------------------------------------------------------------------------------------------
+    'Kraken': {
+        'MATIC': 'MATICEUR',
+        'BTC': 'XBTUSD',
+        'ETH': 'ETHUSD',
+        # Add or modify more assets and trading pairs for Kraken
+    },
+    
+#---------------------------------------------------------------------------------------------------------------
+    # Add or modify more exchanges and their asset mappings here
+}
